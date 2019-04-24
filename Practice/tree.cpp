8b654b814c1a0c57f71cda40df86768ec88cea50
sanchit2843 <sanchittanwar75@gmail.com>
2019-04-24 10:59:52 +0530

    tree print

--- Practice/tree.cpp
+++ Practice/tree.cpp
@@ -1,29 +1,65 @@
 #include <iostream>
 using namespace std;
 
-class node
+struct Node
 {
-public:
-  int d;
-  node*left;
-  node*right;
-
-  node(int d)
-  {
-    data = d;
-    left = NULL;
-    right = NULL;
-  }
+int data;
+Node* left;
+Node* right;
+Node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
 };
 
-node *buildtree(int d)
+Node* buildtree()
 {
-  if(d == -1)
+  int d;
+  cin>>d;
+  if(d==-1)
   {
     return NULL;
   }
-  root = new node(d);
-  root->left = buildtree(root->left);
-  root->right = buildtree(root->right);
+  Node *root = new Node(d);
+  root->left = buildtree();
+  root->right = buildtree();
+  return root;
+}
+void print(Node *root)
+{
+  if(root==NULL)
+    return;
+  cout<<root->data;
+  print(root->left);
+  print(root->right);
+}
+
+void printIN(Node *root)
+{
+  if(root==NULL)
+    return;
+  printIN(root->left);
+  cout<<root->data;
+  printIN(root->right);
+}
+void printPOST(Node *root)
+{
+  if(root==NULL)
+    return;
+  printPOST(root->left);
+  printPOST(root->right);
+
+  cout<<root->data;
+}
+
+int main()
+{
+  Node *root = buildtree();
+  print(root);
+  cout<<endl;
+  printIN(root);
+  cout<<endl;
+  printPOST(root);
 
 }
